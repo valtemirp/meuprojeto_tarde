63e6ba7fb8c22137c3dc62671dbf20dc2ffaca83
Valtemir Procópio de Lima <valtemir.procopio@rn.senac.br>
2023-08-02 11:56:18 -0300

    Alteramos as rotas com render_template

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Olá mundo"
+    return render_template('index.html',titulo = 'Página inicial')
 
 @app.route('/projetos')
 def projetos():
-    return 'Projetos'
+    return render_template('projetos.html', titulo = 'Projetos')
