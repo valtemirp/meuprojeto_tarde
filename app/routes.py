195416730ffb0f73d336ab2a784bdbd04e01af3c
Valtemir Procópio de Lima <valtemir.procopio@rn.senac.br>
2023-08-03 10:20:07 -0300

    Criamos rotas personalizadas

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,12 @@ from flask import render_template
 def index():
     return render_template('index.html',titulo = 'Página inicial')
 
-@app.route('/projetos')
-def projetos():
-    return render_template('projetos.html', titulo = 'Projetos')
+@app.route('/contatos')
+def contatos():
+    return render_template('contatos.html', titulo = 'Contatos')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html', titulo = 'Sobre')
+
+
