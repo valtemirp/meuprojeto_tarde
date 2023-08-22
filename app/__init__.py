a9f7756977b2c15d8441499891829e1d71e8f933
Valtemir Procópio de Lima <valtemir.procopio@rn.senac.br>
2023-08-22 17:28:39 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
+import brazilcep
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
